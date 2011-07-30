ee640bc54805c8cbbd4d3a48b210fa85793dd0e3
Nicolas Lescure <fiber.optic0@gmail.com>
2011-07-30 14:02:20 +0200

    add ezinfo

---
+++ nl_cronjobs/ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+
+class nl_cronjobsInfo
+{
+    static function info()
+    {
+        return array( 'Name' => "NL Cronjobs",
+                      'Version' => "1.4",
+                      'Copyright' => "Nicolas Lecure",
+                      'License' => "GNU General Public License v2.0"
+                     );
+    }
+}
+?>
